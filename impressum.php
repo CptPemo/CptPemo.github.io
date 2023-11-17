5e285e4a3c5eb732f9b6a8bee5bfc453de9da552
CptPemo <132474222+CptPemo@users.noreply.github.com>
2023-11-17 11:28:19 +0100

    footer new

---
+++ impressum.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>Advanced Anatomy</title>
+        <link rel="stylesheet" href="styles/AdvAna.css">
+        <link rel="stylesheet" href="styles/header.css">
+        <link rel="stylesheet" href="styles/main.css">
+        <link rel="stylesheet" href="styles/footer.css">
+        <link rel="stylesheet" href="styles/quizGrid.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;600&display=swap" rel="stylesheet">
+        <link rel="icon" type="image/png" href="bilder/favicon.png">
+        <link rel="icon" type="image/png" href="bilder/favicon1.png">
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+        <header class="header">
+
+            <?php include 'header.html';?>           
+
+        </header>
+         
+        <main>
+
+        
+        
+        
+        </main>
+
+        <footer class="footer">
+            <?php include 'footer.html'; ?>
+            </footer>
+    </body>
+</html>
+
---
+++ kontakt.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <title>Advanced Anatomy</title>
+        <link rel="stylesheet" href="styles/AdvAna.css">
+        <link rel="stylesheet" href="styles/header.css">
+        <link rel="stylesheet" href="styles/main.css">
+        <link rel="stylesheet" href="styles/footer.css">
+        <link rel="stylesheet" href="styles/quizGrid.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;600&display=swap" rel="stylesheet">
+        <link rel="icon" type="image/png" href="bilder/favicon.png">
+        <link rel="icon" type="image/png" href="bilder/favicon1.png">
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+        <header class="header">
+
+            <?php include 'header.html';?>           
+
+        </header>
+         
+        <main>
+
+          <h2>Kontaktformular</h2>
+
+          <form action="php/send_email.php" method="post">
+            <label for="name">Name:</label><br>
+            <input type="text" id="name" name="name"><br>
+            <label for="email">Emailadresse:</label><br>
+            <input type="email" id="email" name="email"><br>
+            <label for="betreff">Betreff:</label><br>
+            <input type="text" id="betreff" name="betreff"><br>
+            <label for="inhalt">Inhalt:</label><br>
+            <textarea id="inhalt" name="inhalt"></textarea><br><br>
+            <input type="submit" value="Absenden">
+          </form>
+        
+        
+        
+        </main>
+
+        <footer class="footer">
+            <?php include 'footer.html'; ?>
+            </footer>
+    </body>
+</html>
+
+
+
