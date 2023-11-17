089e4066feaaf70a5c8ccf19a36692fef00ebb20
CptPemo <132474222+CptPemo@users.noreply.github.com>
2023-11-17 17:05:54 +0100

    j

---
+++ test.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+        <title>Advanced Anatomy</title>
+        <link rel="stylesheet" href="styles/AdvAna.css">
+        <link rel="stylesheet" href="styles/header.css">
+        <link rel="stylesheet" href="styles/main.css">
+        <link rel="stylesheet" href="styles/footer.css">
+        <link rel="stylesheet" href="styles/quizGrid.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="stylesheet" href="styles/Impressum.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;600&display=swap" rel="stylesheet">
+        <link rel="icon" type="image/png" href="bilder/favicon.png">
+        <link rel="icon" type="image/png" href="bilder/favicon1.png">
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+        <header class="header">
+
+            <?php include 'header.html';?>           
+
+        </header>
+         
+        <main>
+
+        <div class="Impressum">
+        <h1>Under Construction</h1>
+
+        <h2>We are putting our efford in getting the german version up and running at the moment</h2>
+        <p>We will launch an english version very soon!<br />
+            If you want, you can register your email here to get notified once we launch!</p>
+
+            <input name="e-mail"> e-mail</input>
+
+        
+        
+        </div>
+        </main>
+
+        <footer class="footer">
+            <?php include 'footer.html'; ?>
+            </footer>
+    </body>
+</html>
+
+
